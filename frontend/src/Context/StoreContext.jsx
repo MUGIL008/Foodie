8befc668fcab440ca317794a99860c9ed23ce515
Mugilanandam <78331455+MUGIL008@users.noreply.github.com>
2025-07-03 20:46:38 +0530

    Update StoreContext.jsx

--- frontend/src/Context/StoreContext.jsx
+++ frontend/src/Context/StoreContext.jsx
@@ -6,7 +6,8 @@ import axios from "axios";
 
 const StoreContextProvider = (props) => {
   const[cartItems,setCartItems]= useState({})
-  const url="http://localhost:4000";
+  // const url="http://localhost:4000";
+  const url = "https://foodie-6h6l.onrender.com";
   const[token,setToken]=useState("")
   const[food_list,Setfood_list]=useState([])
   const addToCart= async(itemId)=>{
